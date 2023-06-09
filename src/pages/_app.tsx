abbc64c807a20331a53343782ebe3b43af14a77b
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2023-06-09 16:40:09 -0300

    Added global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,22 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import GlobalStyles from 'styles/globlal'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Travel the World</title>
+        <meta
+          name="description"
+          content="A site to show pinned places that have already been visited or those that you must visit someday."
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default App
