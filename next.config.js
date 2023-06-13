332f3888cda8a1dc54b2eb6d27a7121d531fc9b8
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2023-06-13 20:07:27 -0300

    Add PWA configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV !== 'production'
+})
+
+const nextConfig = withPWA({
   reactStrictMode: true
-}
+})
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,10 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Travel the World</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
           content="A site to show pinned places that have already been visited or those that you must visit someday."
