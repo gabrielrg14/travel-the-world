91601e91078b334a2847700d6439b3e57c15b312
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2024-05-14 13:50:13 -0300

    Fix Cypress baseUrl configuration property

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   projectId: 'e71ybk',
   e2e: {
-    baseUrl: 'https://travels.gabrielrapucci.com.br',
+    baseUrl: 'http://localhost:3000',
     video: true
   }
 })
